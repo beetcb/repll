cead567b06dcc844e50c98fdb64d4cd2909a4e72
beet <63141491+beetcb@users.noreply.github.com>
2021-01-13 21:59:13 +0800

    feat: test it

---
+++ TEST/test.js
@@ -0,0 +1,15 @@
+const liveRead = require('../lib/liveRead')
+const rt = new liveRead(`GIT > `)
+
+// Listen input key-by-key
+rt.on('key', (key, data) => {
+  rt.refreshPrompt(`${EOL}INPUT: ${key}${EOL}ALL: ${rt.input}`)
+})
+
+// Complete action
+rt.on('complete', () => {
+  const input = rt.input
+  const query = ['feat: ', 'fix: ']
+  const chosen = query.filter((e) => e.startsWith(input)).join(EOL)
+  rt.refreshPrompt(chosen, true)
+})
