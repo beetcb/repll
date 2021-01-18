f14c19a84939172c91064fc73dc1af91528cc82c
beet <63141491+beetcb@users.noreply.github.com>
2021-01-18 19:03:23 +0800

    fix: weird output issue

---
+++ TEST/placeholder.js
@@ -0,0 +1,13 @@
+const { replLive, onLine, onInput, refresh } = require('../index')
+
+// Create a repll instance
+const prompt = `› `
+const repll = replLive(prompt, prompt.length, 'LINE: 1')
+
+onLine(line => {
+  return `LINE: ${line}`
+})
+
+onInput(() => {
+  refresh(`ALL: ${repll.input}`)
+})
