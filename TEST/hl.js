cb158a81a8bb3f6b0bb7b2b32f9e0ba769991345
beet <63141491+beetcb@users.noreply.github.com>
2021-01-24 00:50:52 +0800

    fix(refresh): disable clearup before initalization

---
+++ TEST/hl.js
@@ -0,0 +1,22 @@
+const { replLive, onInput } = require('../index')
+const c = require('chalk')
+
+const repll = replLive(['repll› ', '...'], 'Type const or number to get the heightlight')
+
+onInput(() => {
+  const input = repll.input
+  const define = input.match(/const$/)
+  const number = input.match(/\d+$/)
+  if (define) {
+    heightlight(define[0], 'cyan')
+  }
+  if (number) {
+    heightlight(number[0], 'green')
+  }
+})
+
+function heightlight(string, color) {
+  const { write, clear, refresh } = repll
+  clear(string.length)
+  write(c`{${color} ${string}}`)
+}
